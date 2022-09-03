7e4ce0d99188ee2044361cbcc39266075d00382d
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2022-09-03 00:45:48 -0700

    0.1.0: created misc folder

--- src/5-include_import.py
+++ src/5-include_import.py
@@ -1,10 +1,7 @@
 from jinja2 import Environment, FileSystemLoader
 
-persons = [
-    {'name': 'Alex', 'age': 18, 'weight': 78.5},
-    {'name': 'Niko', 'age': 20, 'weight': 92.1},
-    {'name': 'Ivan', 'age': 39, 'weight': 83.9}
-]
+from src.misc.persons import persons
+
 domain = 'https://www.google.com'
 dialog = {'title': 'Внимание!', 'msg': 'Это тестовый диалог'}
 
