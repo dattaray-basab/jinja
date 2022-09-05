be87d43040982b329d563f2d7cc6e76e6e136202
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2022-09-04 20:21:46 -0700

    expt 4

--- src/basab_expts/5-include_import.py
+++ src/basab_expts/5-include_import.py
@@ -5,6 +5,7 @@ from jinja2 import Environment, FileSystemLoader
 # from src.basab_expts.Filters.str_ops import lower2
 # from src.basab_expts.Filters.str_ops import filter_x1
 from src.basab_expts.Filters.str_ops import filter_x1
+from src.basab_expts.Functions.fn_group_1 import fn_x2
 from src.misc.persons import persons
 
 dialog = {'title': 'basab title'}
@@ -13,6 +14,7 @@ dialog = {'title': 'basab title'}
 file_loader = FileSystemLoader( 'Templates' )
 env = Environment(loader=file_loader)
 env.filters["filter_x1"] = filter_x1
+env.globals["fn_x2"] = fn_x2
 
 tm = env.get_template('content2.html')
 msg = tm.render(users=persons)
---
+++ src/basab_expts/Functions/fn_group_1.py
@@ -0,0 +1,6 @@
+
+
+def fn_x2(txt):
+    return txt.lower()
+
+# print(upper2('Basab'))
