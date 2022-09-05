d01807158e30966c56dbcd60de5496684c8cf4dc
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2022-09-04 21:19:32 -0700

    expt 5

--- soft_gen/source/env_mgt/mgr_env.py
+++ soft_gen/source/env_mgt/mgr_env.py
@@ -1,1 +1,14 @@
+from jinja2 import FileSystemLoader, Environment
+
+from soft_gen.source.Filters.str_ops import filter_x1
+from soft_gen.source.Functions.fn_group_1 import fn_x2
+
+
 def env_mgr():
+    def fn_getenv():
+        file_loader = FileSystemLoader( 'Templates' )
+        env = Environment( loader=file_loader )
+        env.filters["filter_x1"] = filter_x1
+        env.globals["fn_x2"] = fn_x2
+        return env
+    return fn_getenv
--- src/basab_expts/5-include_import.py
+++ src/basab_expts/5-include_import.py
@@ -4,6 +4,7 @@ from jinja2 import Environment, FileSystemLoader
 
 # from src.basab_expts.Filters.str_ops import lower2
 # from src.basab_expts.Filters.str_ops import filter_x1
+from soft_gen.source.code_gen.main import fn_getenv
 from src.basab_expts.Filters.str_ops import filter_x1
 from src.basab_expts.Functions.fn_group_1 import fn_x2
 from src.misc.persons import persons
@@ -11,12 +12,12 @@ from src.misc.persons import persons
 dialog = {'title': 'basab title'}
 # dialog = {'title': 'Внимание!', 'msg': 'Это тестовый диалог'}
 
-def fn_getenv():
-    file_loader = FileSystemLoader( 'Templates' )
-    env = Environment( loader=file_loader )
-    env.filters["filter_x1"] = filter_x1
-    env.globals["fn_x2"] = fn_x2
-    return env
+# def fn_getenv():
+#     file_loader = FileSystemLoader( 'Templates' )
+#     env = Environment( loader=file_loader )
+#     env.filters["filter_x1"] = filter_x1
+#     env.globals["fn_x2"] = fn_x2
+#     return env
 
 
 env = fn_getenv()
