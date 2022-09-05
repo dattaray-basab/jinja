75171e915e7ef6f85c120908e80ce09646c6f13b
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2022-09-04 21:39:56 -0700

    expt 5

--- src/basab_expts/5-include_import.py
+++ src/basab_expts/5-include_import.py
@@ -1,11 +1,13 @@
-from soft_gen.source.code_gen.main import fn_getenv
+# from soft_gen.source.code_gen.main import fn_getenv
+from soft_gen.source.env_mgt.mgr_env import env_mgr
+
 from src.misc.persons import persons
+
+fn_getenv = env_mgr()
 
 env = fn_getenv()
 
-file_path = 'content2.html'
-
-tm = env.get_template(file_path)
+tm = env.get_template('content2.html')
 msg = tm.render({'users': persons})
 
 print(msg)
--- src/basab_expts/Tokens/persons.py
+++ src/basab_expts/Tokens/persons.py
@@ -1,5 +1,11 @@
-persons = [
-    {'name': 'Alex', 'age': 18, 'weight': 78.5},
-    {'name': 'Nikolas', 'age': 20, 'weight': 92.1},
-    {'name': 'Ivan', 'age': 39, 'weight': 83.9}
-]
+def token_mgr():
+    def fn_get_tokens():
+        persons = [
+            {'name': 'Alex', 'age': 18, 'weight': 78.5},
+            {'name': 'Nikolas', 'age': 20, 'weight': 92.1},
+            {'name': 'Ivan', 'age': 39, 'weight': 83.9}
+        ]
+        tokens = {'users': persons}
+        return tokens
+
+    return  fn_get_tokens
