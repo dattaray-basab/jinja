6659a706ab4129bf8176acb8c9ee0193221bae44
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2022-09-05 09:21:16 -0700

    introduced codegen_mgr: 8

--- soft_gen/source/code_generator/mgr_codegen.py
+++ soft_gen/source/code_generator/mgr_codegen.py
@@ -1,31 +1,52 @@
 import os
+import pickle
+import shutil
 
 from soft_gen.source.common.constants import RAW_DIRPATH, SOURCE_DIRPATH
 from soft_gen.source.env_mgt.mgr_env import env_mgr
 from src.basab_expts.Tokens.mgr_token import token_mgr
 
 
+
 def codegen_mgr(caller_filepath):
     _raw_dir_path = os.path.join(os.path.dirname(caller_filepath), RAW_DIRPATH)
     _source_dir_path = os.path.join( os.path.dirname( caller_filepath ), SOURCE_DIRPATH )
     if os.path.exists(_source_dir_path):
-        os.removedirs(_source_dir_path)
+        shutil.rmtree( _source_dir_path )
     os.mkdir(_source_dir_path)
 
     fn_get_tokens = token_mgr()
     tokens = fn_get_tokens()
     fn_getenv = env_mgr()
 
+    def fn_pickle_writer(file_path, data):
+        dir_path = os.path.dirname(file_path)
+        if os.path.exists( dir_path ):
+            shutil.rmtree( dir_path )
+        os.mkdir( _source_dir_path )
+
+        # with open(file_path, "wb") as handle:
+        #     pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        with open( file_path, 'w' ) as f:
+            f.write( data )
+
     def _fn_generate_code_for_file(dirpath, filename):
         try:
             env = fn_getenv(dirpath)
             tm = env.get_template( filename )
-            msg = tm.render( tokens )
-            print(msg)
+            data = tm.render( tokens )
+            print(data)
 
-            suffix_source_dirpath, _ = dirpath.rsplit('/', 1)
-            source_dirpath = os.path.join( suffix_source_dirpath, SOURCE_DIRPATH)
-            print(source_dirpath)
+            # suffix_source_dirpath, _ = dirpath.rsplit('/', 1)
+            # source_dirpath = os.path.join( suffix_source_dirpath, SOURCE_DIRPATH)
+            source_dirpath = dirpath.replace(RAW_DIRPATH, SOURCE_DIRPATH)
+
+            source_filepath = os.path.join(source_dirpath, filename)
+
+            fn_pickle_writer( source_filepath, data )
+
+            # print(source_dirpath)
             x = 1
         except Exception as x:
             print(x)
