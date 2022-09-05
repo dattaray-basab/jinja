9457af7e064e6d4c42d1fef14dd6d0057a72a487
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2022-09-04 21:46:00 -0700

    introduced soft_gen abstraction

--- src/basab_expts/5-include_import.py
+++ src/basab_expts/5-include_import.py
@@ -1,13 +1,17 @@
 # from soft_gen.source.code_gen.main import fn_getenv
 from soft_gen.source.env_mgt.mgr_env import env_mgr
+from src.basab_expts.Tokens.mgr_token import token_mgr
 
 from src.misc.persons import persons
 
 fn_getenv = env_mgr()
+fn_get_tokens = token_mgr()
+
+tokens = fn_get_tokens()
 
 env = fn_getenv()
 
 tm = env.get_template('content2.html')
-msg = tm.render({'users': persons})
+msg = tm.render(tokens)
 
 print(msg)
