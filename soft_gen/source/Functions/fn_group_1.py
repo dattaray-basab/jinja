8161b969e2606cd1c0d3caa9d32095354e24671e
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2022-09-04 21:16:38 -0700

    expt 5

---
+++ soft_gen/source/Functions/fn_group_1.py
@@ -0,0 +1,6 @@
+
+
+def fn_x2(txt, prefix = 'PREFIX:: '):
+    return prefix + txt.lower()
+
+# print(upper2('Basab'))
--- soft_gen/source/code_gen/main.py
+++ soft_gen/source/code_gen/main.py
@@ -4,12 +4,13 @@ from jinja2 import Environment, FileSystemLoader
 
 # from src.basab_expts.Filters.str_ops import lower2
 # from src.basab_expts.Filters.str_ops import filter_x1
-from src.basab_expts.Filters.str_ops import filter_x1
-from src.basab_expts.Functions.fn_group_1 import fn_x2
+# from src.basab_expts.Filters.str_ops import filter_x1
+# from src.basab_expts.Functions.fn_group_1 import fn_x2
+from soft_gen.source.Filters.str_ops import filter_x1
+from soft_gen.source.Functions.fn_group_1 import fn_x2
+
 from src.misc.persons import persons
 
-dialog = {'title': 'basab title'}
-# dialog = {'title': 'Внимание!', 'msg': 'Это тестовый диалог'}
 
 def fn_getenv():
     file_loader = FileSystemLoader( 'Templates' )
